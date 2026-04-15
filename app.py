58c0dd47f877e3c0615715118e22713986bea877
Lavanya S <lavanyasatha66@gmail.com>
2026-04-15 20:12:33 +0530

    Change default port from 5000 to 10000

--- app.py
+++ app.py
@@ -52,7 +52,8 @@ def index():
         job_description=job_description,
         filename=filename
     )
+    
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
